03037a8404f4b0fd02253ea7836ef90d4e62f760
Camille BARBIT <cbarbit@e1r12p16.42.fr>
2022-01-18 14:46:10 +0100

    new version

--- client.c
+++ client.c
@@ -6,10 +6,11 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 16:07:46 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/01/18 11:29:04 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/01/18 14:43:51 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//je definis 0 quand tout va bien et 1 quand il y a une erreur
 
 /* Le client va envoyer un message au server sous forme de signaux a dechiffrer.
 Je vais devoir determiner par exemple que mon SIGUSR1 vaut 0 et mon SIGUSR2 vaut 1.
@@ -23,16 +24,54 @@ void	char_to_binary(char c, pid_t pid) // ou int pid?
 
 }
 
-/* il faut gerer les erreurs - lorsque j'execute, je dois avoir trois arguments.
-le pid ne doit contenir que des chiffres. -> penser a faire une fonction qui check si le pid est bien compose uniquement
-de chiffres.
+int	ft_is_digit(char *str) //cette fonction sert a gerer que mon pid est bien compose uniquement de chiffres!
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if(!(i >= 0 && i <= 9))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+int	ft_check_parameters(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		ft_printf("Minitalk cannot work: Too few arguments!");
+		return (1);
+	}
+	if (argc > 3)
+	{
+		ft_printf("Minitalk cannot work: Too many arguments!");
+		return (1);
+	}
+	if ((ft_is_digit(argv[1]) == 1)|| (ft_atoi(argv[1]) < 0)) //si mon pid contient autre chose que des chiffres ou si pid < 0
+	{
+		ft_printf("Minitalk cannot work : There is an error with the PID!");
+		return (1);
+	else
+		return (0);
+}
+
+/*
 Peut-etre y-a-t-il d'autres elements a prendre en compte en gestion d'erreur?
 
 A chaque fois que j'utilise la fonction kill, je dois utiliser la fonction usleep.
 
 Dans cette partie, je vais devoir utiliser la fonction kill(pid_t pid, int sig) qui est la fonction qui
 permet au client d'envoyer un signal au serveur.
-
-
-
  */
+
+int	main(int argc, char **argv)
+{
+	if (ft_check_parameters(argc, argv) == 1)
+		return (1);
+	if (argc == 3)
+	{
+
+	}
+}
