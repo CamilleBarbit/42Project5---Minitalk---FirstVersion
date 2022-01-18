6ba655986beaa56f5c3fda7468cbf4a6a306d014
Camille BARBIT <cbarbit@e1r12p16.42.fr>
2022-01-18 12:33:26 +0100

    client.c

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 16:07:46 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/01/14 16:57:29 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/01/18 11:29:04 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,16 @@ void	char_to_binary(char c, pid_t pid) // ou int pid?
 
 }
 
-//il faut gerer les erreurs - lorsque j'execute, je dois avoir trois arguments.
-//le pid ne doit contenir que des chiffres. -> penser a faire une fonction qui check si le pid est bien compose uniquement de chiffres.
-//Peut-etre y-a-t-il d'autres elements a prendre en compte en gestion d'erreur?
+/* il faut gerer les erreurs - lorsque j'execute, je dois avoir trois arguments.
+le pid ne doit contenir que des chiffres. -> penser a faire une fonction qui check si le pid est bien compose uniquement
+de chiffres.
+Peut-etre y-a-t-il d'autres elements a prendre en compte en gestion d'erreur?
 
-//a chaque fois que j'utilise la fonction kill, je dois utiliser la fonction usleep.
+A chaque fois que j'utilise la fonction kill, je dois utiliser la fonction usleep.
+
+Dans cette partie, je vais devoir utiliser la fonction kill(pid_t pid, int sig) qui est la fonction qui
+permet au client d'envoyer un signal au serveur.
+
+
+
+ */
