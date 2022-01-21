c6e014491dfd158375cfc1eee72b08b33d6c36c5
Camille BARBIT <cbarbit@e1r12p11.42.fr>
2022-01-21 12:32:25 +0100

    new version of server

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 14:41:52 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/01/21 10:15:43 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/01/21 12:30:55 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,39 +21,55 @@ Une fois que j'ai récupéré le PID du processus serveur, je dois l'afficher da
 J'ai deux options: soit je transforme le PID (qui est un nombre) en char via la fonction ITOA, ou je l'affiche via la fonction PUTNBR.
 */
 
-void    ft_handle_signals(int signum, siginfo_t *info, void *notuseful)
-{
-    static int  i;
-    static int  j;
-    static char str[100000];
-    (void)info; //je n'utilise pas ce parametre
-    (void)notuseful; //je n'utilise pas ce parametre
 
-    if (i = 0)
-        str[j] = '\0';
-    if (i < 8)
+void    ft_handle_signals(int signum, siginfo_t *info, void *useless)
+{
+    static int  i; //le i correspond au nombre de signaux que je vais recevoir.
+
+    i = 0;
+    while (i <= 32)
     {
-        str[j] = str[j] << 1;
-        if (signum == SIGUSR2)
-            str[j] = str[j] + 1;
-        if (signum == SIGUSR1)
-            str[j] = str[j] + 0;
-        i++;
-    }
-    if (i == 8)
-    {
-        if (!str[j] || j = 99999)
-        {
-            write(1, str, j);
-            str[0] = '\0';
-            i = 0;
-            j = 0;
-            return ;
-        }
-        i = 0;
-        j++;
+        //je dois envoyer dans une autre fonction qui va modifier le signal recu et stocker les 32 premiers bits dans un int
     }
 }
+
+void    binary_to_int(int signum)
+{
+    
+}
+// void    ft_handle_signals(int signum, siginfo_t *info, void *useless)
+// {
+//     static int  i;
+//     static int  j;
+//     static char str[100000];
+//     (void)info; //je n'utilise pas ce parametre
+//     (void)useless; //je n'utilise pas ce parametre
+
+//     if (i = 0)
+//         str[j] = '\0';
+//     if (i < 8)
+//     {
+//         str[j] = str[j] << 1;
+//         if (signum == SIGUSR2)
+//             str[j] = str[j] + 1;
+//         if (signum == SIGUSR1)
+//             str[j] = str[j] + 0;
+//         i++;
+//     }
+//     if (i == 8)
+//     {
+//         if (!str[j] || j = 99999)
+//         {
+//             write(1, str, j);
+//             str[0] = '\0';
+//             i = 0;
+//             j = 0;
+//             return ;
+//         }
+//         i = 0;
+//         j++;
+//     }
+// }
 
 int main()
 {
